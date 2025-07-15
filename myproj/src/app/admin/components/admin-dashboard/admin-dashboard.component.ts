3a06b902bc393ac51d9bde7a192e0d67825b7d89
AK12210 <156789576+AK12210@users.noreply.github.com>
2025-07-15 09:00:55 +0500

    Add files via upload

--- myproj/src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ myproj/src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-dashboard',
   imports: [
-    RouterLink
+    RouterLink,
+    NgxPaginationModule,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent{
   username: string = 'Adminov Admin';
+    items = Array.from({ length: 50 }, (_, i) => ({
+    id: i + 230001,
+    code: "AD",
+    name: "Active directory",
+    description: "very interesting",
+    comment: "to do tomorrow",
+    isActual: true,
+    isCritical: false,
+    createdDate: "2025-03-28"
+  }));
+  page = 1;
+   itemsPerPage = 10;
+
+  perPageOptions = [5, 10, 20, 50];
 }
