49ee1afe19576ede1d16a92a8cbefc6f9bc62cea
AK12210 <156789576+AK12210@users.noreply.github.com>
2025-07-28 09:26:53 +0500

    Add files via upload

--- myproj/src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ myproj/src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -8,6 +8,15 @@ import {NzIconDirective} from 'ng-zorro-antd/icon';
 import { UserService, MyUser } from './user.service';
 import {NzModalComponent} from 'ng-zorro-antd/modal';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import {NzAutosizeDirective} from 'ng-zorro-antd/input';
+import {NzRowDirective} from 'ng-zorro-antd/grid';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import {NzSelectComponent} from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -20,7 +29,15 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
     NzIconDirective,
     ReactiveFormsModule,
     NzModalComponent,
-    NzButtonModule
+    NzButtonModule,
+    NzAutosizeDirective,
+    NzFormModule,
+    NzInputModule,
+    NzRowDirective,
+    NzSelectModule,
+    NzDatePickerModule,
+    NzDrawerModule,
+    NzSelectComponent
   ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css',
@@ -30,8 +47,23 @@ export class AdminDashboardComponent implements OnInit{
   username: string = 'Adminov Admin';
   showCreateForm = false;
    createForm!: FormGroup;
+   visible = false;
+   isEditDrawerVisible = false;
+editForm!: FormGroup;
+selectedUserId!: number;
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
+    createMessage(type: string): void {
+    this.message.create(type, `User deleted successfully`);
+  }
   users: MyUser[] = [];
-  constructor(private fb: FormBuilder, private userService: UserService) {}
+  constructor(private fb: FormBuilder, private userService: UserService, private message: NzMessageService) {}
   items = Array.from({ length: 50 }, (_, i) => ({
     id: i + 230001,
     code: "AD",
@@ -64,6 +96,10 @@ ngOnInit(): void {
       roles: [''],
       active: [false]
     });
+     this.editForm = this.fb.group({
+    username: [''],
+    roles: ['']
+  });
   }
 createUser() {
   const newUser = this.createForm.value;
@@ -72,10 +108,35 @@ createUser() {
       this.loadUsers();
       this.createForm.reset();
       this.showCreateForm = false;
+      this.message.create('success', `User created successfully`);
     },
     error: err => console.error(err)
   });
 }
+openEditDrawer(user: MyUser): void {
+  this.selectedUserId = user.id!;
+  console.log('Opening drawer with user:', user);
+  this.editForm.patchValue({
+    username: user.username,
+    roles: user.roles
+  });
+  this.isEditDrawerVisible = true;
+}
+
+closeEditDrawer(): void {
+  this.isEditDrawerVisible = false;
+}
+
+submitEdit(): void {
+  if (this.editForm.valid) {
+    const updatedUser = this.editForm.value;
+    this.userService.updateUser(this.selectedUserId, updatedUser).subscribe(() => {
+      this.loadUsers();
+      this.closeEditDrawer();
+    });
+  }
+}
+
   page = 1;
    itemsPerPage = 10;
   loading = true;
