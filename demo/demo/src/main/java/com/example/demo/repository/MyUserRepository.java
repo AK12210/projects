95cfdc7e68be44ae8098ce57aef213fc1bb34c32
AK12210 <156789576+AK12210@users.noreply.github.com>
2025-07-29 09:38:48 +0500

    Update MyUserRepository.java

--- demo/demo/src/main/java/com/example/demo/repository/MyUserRepository.java
+++ demo/demo/src/main/java/com/example/demo/repository/MyUserRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface MyUserRepository extends JpaRepository<MyUser, Long> {
 
     Optional<MyUser> findByUsername(String username);
+    List<MyUser> findByUsernameContainingIgnoreCaseOrRolesContainingIgnoreCase(String username, String roles);
 }
